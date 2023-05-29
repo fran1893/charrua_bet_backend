5c83d3edca89bb7981ac1b469d0a2ca2018f616e
Francisco Diaz <diazsalerno2@outlook.es>
2023-05-29 14:26:54 +0200

    Add: user auth route

--- router.js
+++ router.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const indexRouter = require("./routes/index");
+const authRouter = require("./routes/auth");
 
 /* home page */
 router.use("/", indexRouter);
+
+/* authentication */
+router.use("/auth", authRouter);
 
 module.exports = router;

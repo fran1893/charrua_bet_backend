e4fba54a7527466222df7dac1e9c5317f858c731
Francisco Diaz <diazsalerno2@outlook.es>
2023-06-02 14:29:50 +0200

    Add: update payment endpoint

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -5,9 +5,11 @@ const {
   sendErrorResponse,
 } = require("../_util/sendResponse");
 const { errorMsg, successMsg } = require("../_util/messages");
+const { where } = require("sequelize");
 
 const paymentController = {};
 
+// ADD PAYMENT TO A GAME
 paymentController.addPayment = async (req, res) => {
   try {
     const { amount, game_id } = req.body;
@@ -21,6 +23,32 @@ paymentController.addPayment = async (req, res) => {
     sendSuccsessResponse(res, 200, successMsg.payment.CREATE);
   } catch (error) {
     sendErrorResponse(res, 500, errorMsg.payment.CREATE, error);
+  }
+};
+
+// UPDATE PAYMENT ON A GAME
+paymentController.updatePayment = async (req, res) => {
+  try {
+    const paymentId = req.params.payment_id;
+    const { amount } = req.body;
+    const workspace_id = req.user_workspace;
+    const payment = await Payment.findByPk(paymentId);
+
+    if (workspace_id == payment.workspace_id) {
+      const updatePayment = await Payment.update(
+        { amount: amount },
+        { where: { id: paymentId } }
+      );
+      if (updatePayment == 1) {
+        return sendSuccsessResponse(res, 200, successMsg.payment.UPDATE);
+      } else {
+        return sendErrorResponse(res, 400, errorMsg.payment.REQUIERED);
+      }
+    } else {
+      sendErrorResponse(res, 401, errorMsg.authorization.NOAUTH);
+    }
+  } catch (error) {
+    sendErrorResponse(res, 500, errorMsg.payment.UPDATE);
   }
 };
 
--- routes/payment.js
+++ routes/payment.js
@@ -4,6 +4,9 @@ const isAdmin = require("../middlewares/isAdmin");
 
 const router = require("express").Router();
 
+// ADD PAYMENT TO A GAME
 router.post("/create", verifyToken, isAdmin, paymentController.addPayment);
+// UPDATE PAYMENT ON A GAME
+router.put("/update/:payment_id", verifyToken, isAdmin, paymentController.updatePayment);
 
 module.exports = router;
